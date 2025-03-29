58d9417d159ddd0bcf216d4c9d5a83d52a2319af
Haschwallth <164790980+Haschwallth@users.noreply.github.com>
2025-03-28 21:54:54 -0300

    Add files via upload

---
+++ calculadora soma.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+
+    int x, y;
+    char resultado;
+
+    printf("escreva aqui o primeiro numero: ");
+    scanf("%i",&x);
+
+     printf("escreva aqui o segundo numero: ");
+    scanf("%i",&y);
+
+    resultado = x + y;
+
+    printf("\nseu resultado e %i", resultado);
+
+
+}
