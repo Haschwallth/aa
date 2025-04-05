6ca473fdc7cc7951d95ddcf9d8a70f2f71721378
Haschwallth <164790980+Haschwallth@users.noreply.github.com>
2025-04-04 22:17:11 -0300

    Add files via upload

---
+++ incompleto.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int choice;
+
+    ESCOLHA:
+
+
+    printf ("Escolha um genero de filme:");
+
+    printf ("\n1 Acao");
+    printf ("\n2 Comedia");
+    printf ("\n3 Drama");
+    printf ("\n4 Ficcao Cientifica");
+    printf ("\n5 Terror");
+
+    scanf("\n\n %d", &choice);
+
+
+
+
+    printf("\n\n");
+    return 0;
+}
